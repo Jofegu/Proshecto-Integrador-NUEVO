e5702ecf9cbf9915a840ac88b6c5229da0cf24a1
Nftalio <joc.guzman98@gmail.com>
2022-08-08 08:40:40 -0400

    asd

--- pijg/src/app/app.module.ts
+++ pijg/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardYSoftSkillsComponent } from './components/hard-y-soft-skills/hard-y-soft-skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { HardYSoftSkillsComponent } from './components/hard-y-soft-skills/hard-y
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HardYSoftSkillsComponent
+    HardYSoftSkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
